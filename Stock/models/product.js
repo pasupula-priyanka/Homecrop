115070012e58246e76c1a8c4327e51173c4cc6b7
pasupula-priyanka <65830128+pasupula-priyanka@users.noreply.github.com>
2023-12-28 12:02:37 +0530

    Update product.js

--- Stock/models/product.js
+++ Stock/models/product.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  category: { type: String, enum: ['Plant Care', 'Grow Bags', 'Packing Materials', 'Potting Mix'], required: true }
+  category: { type: String, enum: ['Plant Care', 'Grow Bags', 'Packing Materials', 'Potting Mix','Rolls'], required: true }
 })
 
 const Product = mongoose.model('Product', productSchema);
